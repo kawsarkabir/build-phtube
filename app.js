d5c14928b1273ae9a1d1c81c4a5c9400407ecda9
kawsarkabir <devkawsarkabir@gmail.com>
2023-09-01 00:07:09 +0600

    single data load and display show data

--- app.js
+++ app.js
@@ -8,12 +8,38 @@ const handleCategories = async()=>{
 // display category 
 const displayCategories = (categories)=>{
     const categoriesContainer = document.getElementById('categories-container');
-    categories.forEach(SingleCategories=>{
+    categories.forEach(singleCategories=>{
         const div = document.createElement('div');
         div.innerHTML = ` 
-        <button class="py-2 px-6 bg-gray-100 rounded-lg">${SingleCategories.category}</button>
+        <button onclick="singleCategoryLoad('${singleCategories.category_id}')" class="py-2 px-6 bg-gray-100 rounded-lg">${singleCategories.category}</button>
         `
         categoriesContainer.appendChild(div);
+    })
+}
+// single category data load
+const singleCategoryLoad= async(categoryId)=>{
+    console.log(categoryId);
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const data = await res.json();
+    const categoryItem = data.data;
+    displaySingleCategory(categoryItem);
+}
+// display single category 
+const displaySingleCategory = (categoryItem) =>{
+    const singleCategoryContainer = document.getElementById('single-category-container');
+    singleCategoryContainer.textContent = ''
+    categoryItem.forEach(singleCategory=>{
+        console.log(singleCategory);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card w-96">
+  <figure><img src="${singleCategory.thumbnail}" alt="Shoes" class="rounded-xl" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">${singleCategory.title}</h2>
+  </div>
+</div>
+        `
+        singleCategoryContainer.appendChild(div)
     })
 }
 handleCategories();

4380bbbca780d105b3a012debdec3174066a22f9
kawsarkabir <devkawsarkabir@gmail.com>
2023-09-01 00:55:56 +0600

    single card done with responsive

--- app.js
+++ app.js
@@ -1,45 +1,61 @@
 // category data load
-const handleCategories = async()=>{
-    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
-    const data = await res.json();
-    const categories =data.data;
-    displayCategories(categories)
-}
-// display category 
-const displayCategories = (categories)=>{
-    const categoriesContainer = document.getElementById('categories-container');
-    categories.forEach(singleCategories=>{
-        const div = document.createElement('div');
-        div.innerHTML = ` 
+const handleCategories = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await res.json();
+  const categories = data.data;
+  displayCategories(categories);
+};
+// display category
+const displayCategories = (categories) => {
+  const categoriesContainer = document.getElementById("categories-container");
+  categories.forEach((singleCategories) => {
+    const div = document.createElement("div");
+    div.innerHTML = ` 
         <button onclick="singleCategoryLoad('${singleCategories.category_id}')" class="py-2 px-6 bg-gray-100 rounded-lg">${singleCategories.category}</button>
-        `
-        categoriesContainer.appendChild(div);
-    })
-}
+        `;
+    categoriesContainer.appendChild(div);
+  });
+};
 // single category data load
-const singleCategoryLoad= async(categoryId)=>{
-    console.log(categoryId);
-    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
-    const data = await res.json();
-    const categoryItem = data.data;
-    displaySingleCategory(categoryItem);
-}
-// display single category 
-const displaySingleCategory = (categoryItem) =>{
-    const singleCategoryContainer = document.getElementById('single-category-container');
-    singleCategoryContainer.textContent = ''
-    categoryItem.forEach(singleCategory=>{
-        console.log(singleCategory);
-        const div = document.createElement('div');
-        div.innerHTML = `
+const singleCategoryLoad = async (categoryId) => {
+  console.log(categoryId);
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
+  );
+  const data = await res.json();
+  const categoryItem = data.data;
+  displaySingleCategory(categoryItem);
+};
+// display single category
+const displaySingleCategory = (categoryItem) => {
+  const singleCategoryContainer = document.getElementById(
+    "single-category-container"
+  );
+  singleCategoryContainer.textContent = "";
+  categoryItem.forEach((singleCategory) => {
+    console.log(singleCategory);
+    const div = document.createElement("div");
+    div.innerHTML = `
         <div class="card w-96">
-  <figure><img src="${singleCategory.thumbnail}" alt="Shoes" class="rounded-xl" /></figure>
-  <div class="card-body">
-    <h2 class="card-title">${singleCategory.title}</h2>
-  </div>
-</div>
-        `
-        singleCategoryContainer.appendChild(div)
-    })
-}
+        <figure><img src="${singleCategory.thumbnail}" alt="Shoes" class="rounded-xl w-80 h-52" /></figure>
+        <div class="card-body flex-row gap-x-5">
+        <div>
+            <img class="w-10 h-10 rounded-full" src="${singleCategory.authors[0].profile_picture}">
+        </div>
+        <div>
+            <h3 class ="text-xl font-semibold">${singleCategory.title}</h3>
+            <div class="flex items-center gap-x-2">
+                 <h4 class="text-gray-500 text-base">${singleCategory.authors[0].profile_name}</h4>
+                 <h4>${singleCategory.authors[0].verified}</h4>
+            </div>
+            <h4 class ="text-gray-400">${singleCategory.others.views} views</h4>
+        </div>
+        </div>
+        </div>
+        `;
+    singleCategoryContainer.appendChild(div);
+  });
+};
 handleCategories();
